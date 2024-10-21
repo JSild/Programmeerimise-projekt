6bd7ca3a6e1274a3d4f2763729ffef5b22d317d9
Jasper Suursild <suursildjasper@gmail.com>
2024-10-21 03:41:39 +0300

    Esimene commit

---
+++ programm.py
@@ -0,0 +1,15 @@
+################################################
+# Programmeerimine I
+# 2024/2025 sügissemester
+#
+# Projekt Vare
+# Teema:
+#
+#
+# Autorid:
+#
+# mõningane eeskuju:
+#
+# Lisakommentaar (nt käivitusjuhend):
+#
+##################################################
