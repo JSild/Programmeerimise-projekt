c3fda6ff23dc5ac9934c6f2341c4b05051091f52
Jasper Suursild <suursildjasper@gmail.com>
2024-10-30 23:46:57 +0200

    v1 mis on aeglane, aga töötab

--- ai/extract_info.py
+++ ai/extract_info.py
@@ -1,21 +1,51 @@
 import requests
 from bs4 import BeautifulSoup
 from urllib.parse import urljoin #lisab base urlile juurde valuesid
-
-## TO-DO
-## 1. extracti lingid kõigilt 
+import datetime
 
 
-#1. add url agent (OPTIONAL)
-#headers = {}
+def getinfo_Retseptisahtel(x):
+    #funktsioon millega eraldada retsepti leheküljelt koostisained
+    def getinfo_from_link(link):
+        start = datetime.datetime.now()
+        r = s.get(link)
+        finish = datetime.datetime.now() - start
+        print(f'Lingi fetchimine {finish}')
+        start = datetime.datetime.now()
+        soup = BeautifulSoup(r.text, 'html.parser')
+        ul_tag = soup.find('ul', class_='td-arrow-list')
+        if ul_tag:
+            ingredients = []
+            for li in ul_tag.find_all('li'):
+                ingredients.append(li.get_text())
+            finish = datetime.datetime.now() - start
+            print(f'Lingi parsimine {finish}')
+            return ingredients
+        else:
+            return False
 
-#2. Base URL
-url = 'https://retseptisahtel.ee/retseptid/'
+    #base url kus x = leheküljenumber
+    url = f'https://retseptisahtel.ee/retseptid/page/{x}/'
+    r = s.get(url)
+    soup = BeautifulSoup(r.text, 'html.parser')
+    #eraldan leheküljelt retseptide lingid
+    h3_tags = soup.find_all('h3', class_='entry-title td-module-title')
+    a_tags = [a_tag.find('a') for a_tag in h3_tags]
+    for link in a_tags:
+        link = link.get('href')
+        info = getinfo_from_link(link)
+        if info:
+            print(info)
 
-#3. cycle through pages
-while True:
-    #5. Send get() req and fetch contents
-    response = requests.get(url)
-    soup = BeautifulSoup(response.content, 'html.parser')
-    footer = soup.select_one('span.pages')
-    print(footer.text.strip())
+    return
+
+# no session 0:00:07.329809
+# with session 0:00:05.461197
+if __name__ == '__main__':
+    s = requests.Session()
+    start = datetime.datetime.now()
+    for i in range(24, 27):
+        getinfo_Retseptisahtel(i)
+        print(i)
+    finish = datetime.datetime.now() - start
+    print(finish)
