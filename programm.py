15d1355bef32f1fe450b10f72445cafc142d83d1
Hardy Kaasik <hardykaasik@Hardys-Laptop.local>
2024-10-30 16:03:27 +0200

    esimene commit

--- programm.py
+++ programm.py
@@ -13,3 +13,17 @@
 # Lisakommentaar (nt käivitusjuhend):
 #
 ##################################################
+
+import requests
+from bs4 import BeautifulSoup 
+
+
+url = 'https://www.santamariaworld.com/ee/retseptid/'
+html = requests.get(url)
+
+soup = BeautifulSoup(html.text, 'html.parser')
+
+lingid = soup.find_all('a', class_ = 'sm-recipe-card__image-link')
+
+for link in lingid:
+    print(link.get('href'))
