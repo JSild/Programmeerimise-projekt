960ac4f2d46bbf6ce081989995b19f2fd936d830
Jasper Suursild <suursildjasper@gmail.com>
2024-10-31 02:24:40 +0200

    v2 mis toimib ja kirjutab tulemused faili ingredients.txt

--- ai/extract_info.py
+++ ai/extract_info.py
@@ -1,29 +1,30 @@
 import requests
 from bs4 import BeautifulSoup
-from urllib.parse import urljoin #lisab base urlile juurde valuesid
 import datetime
+import asyncio
+import aiohttp
 
+ #funktsioon millega eraldada retsepti leheküljelt koostisained
+async def getinfo_from_links(links):
+    async with aiohttp.ClientSession() as session:
+        tasks = [fetch_and_parse(session, link) for link in links]
+        results = await asyncio.gather(*tasks)
+        return results
 
-def getinfo_Retseptisahtel(x):
-    #funktsioon millega eraldada retsepti leheküljelt koostisained
-    def getinfo_from_link(link):
-        start = datetime.datetime.now()
-        r = s.get(link)
-        finish = datetime.datetime.now() - start
-        print(f'Lingi fetchimine {finish}')
-        start = datetime.datetime.now()
-        soup = BeautifulSoup(r.text, 'html.parser')
+async def fetch_and_parse(session, link):
+    async with session.get(link, ssl=False) as response:
+        r_text = await response.text()
+        soup = BeautifulSoup(r_text, 'html.parser')
         ul_tag = soup.find('ul', class_='td-arrow-list')
         if ul_tag:
             ingredients = []
             for li in ul_tag.find_all('li'):
                 ingredients.append(li.get_text())
-            finish = datetime.datetime.now() - start
-            print(f'Lingi parsimine {finish}')
-            return ingredients
+            return (ingredients, link)
         else:
             return False
 
+def getinfo_Retseptisahtel(x):
     #base url kus x = leheküljenumber
     url = f'https://retseptisahtel.ee/retseptid/page/{x}/'
     r = s.get(url)
@@ -31,21 +32,36 @@ def getinfo_Retseptisahtel(x):
     #eraldan leheküljelt retseptide lingid
     h3_tags = soup.find_all('h3', class_='entry-title td-module-title')
     a_tags = [a_tag.find('a') for a_tag in h3_tags]
+    links = []
     for link in a_tags:
-        link = link.get('href')
-        info = getinfo_from_link(link)
+        links.append(link.get('href'))
+    #annan kõik leitud lingid funktsiooni getinfo_from_links, mis leiab info kõikidelt lehtedelt samaaegselt
+    results = asyncio.run(getinfo_from_links(links))
+    page_info = []
+    for info in results:
         if info:
-            print(info)
-
-    return
+            page_info.append(info)
+    if page_info:
+        return page_info
+    else:
+        return
 
 # no session 0:00:07.329809
 # with session 0:00:05.461197
+# total time ?
 if __name__ == '__main__':
     s = requests.Session()
     start = datetime.datetime.now()
-    for i in range(24, 27):
-        getinfo_Retseptisahtel(i)
-        print(i)
+    with open('ingredients.txt', 'w', encoding='UTF-8') as file:
+        for i in range(1, 90):
+            page_info = getinfo_Retseptisahtel(i)
+            if page_info:
+                file.write(f'Page {i}\n')
+                for n, (ingredients, link) in enumerate(page_info, 1):
+                    ingredients_str = ", ".join(ingredients)
+                    file.write(f'{n} - {ingredients} | {link}\n')
+                file.write('\n\n')
+                print(i)
+
     finish = datetime.datetime.now() - start
     print(finish)
